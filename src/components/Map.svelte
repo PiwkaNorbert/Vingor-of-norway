5de393e7aa14f2c2b81cb8a5852f2feda69495be
Norbert Piwka <piwkanorbert@gmail.com>
2023-10-27 15:25:57 +0200

    map with svelte and leaflet

---
+++ src/components/Map.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { setMap } from '../map';
+
+  export let location: {
+    latitude: number;
+    longitude: number;
+  };
+  console.log(location);
+  
+  export let zoom: number = 19;
+  export let markerMarkup: string = '';
+
+  const { latitude, longitude } = location;
+
+  
+</script>
+
+<figure use:setMap={{ latitude, longitude, zoom, markerMarkup }} />
+
+<style>
+  figure {
+    width: 38rem;
+    height: 21rem;
+    box-shadow: var(--shadow-elevation-low);
+  }
+</style>
--- src/content/discover-more/card-1.md
+++ src/content/discover-more/card-1.md
@@ -3,5 +3,5 @@ tag: Story
 title: About us
 image: { src: "/images/about-us.jpg", alt: "A picture of the Owners" }
 description: Learn more about our story and how we started.
-link: "/about-us"
+link: "/about"
 ---
